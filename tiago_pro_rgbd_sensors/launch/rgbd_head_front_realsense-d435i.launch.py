dc47fc6da6dc03d73f90d2093724ba51e7a43589
Noel Jimenez <noel.jimenez@pal-robotics.com>
2025-07-10 11:06:43 +0200

    Add diagnostic analyzers

--- tiago_pro_rgbd_sensors/launch/rgbd_head_front_realsense-d435i.launch.py
+++ tiago_pro_rgbd_sensors/launch/rgbd_head_front_realsense-d435i.launch.py
@@ -5,6 +5,10 @@
 # nondisclosure agreement with PAL Robotics SL. In this case it may not be
 # copied or disclosed except in accordance with the terms of that agreement.
 
+import os
+
+from ament_index_python import get_package_share_directory
+
 import rclpy
 from rclpy.node import Node as RclpyNode
 from launch import LaunchDescription
@@ -51,4 +55,18 @@ def generate_launch_description():
     )
 
     ld.add_action(camera_components)
+
+    rgbd_analyzer = Node(
+        package='diagnostic_aggregator',
+        executable='add_analyzer',
+        namespace='tiago_pro_rgbd_sensors',
+        output='screen',
+        emulate_tty=True,
+        parameters=[
+            os.path.join(
+                get_package_share_directory('tiago_pro_rgbd_sensors'),
+                'config', 'rgbd_analyzers.yaml')],
+    )
+    ld.add_action(rgbd_analyzer)
+
     return ld
